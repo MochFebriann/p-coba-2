5853c0a65737779842de13f86b6327f4fce110fd
MochFebriann <firhanmartono@gmail.com>
2022-07-27 08:15:23 +0700

    first commit

---
+++ src/inheritance1/hasil.java
@@ -0,0 +1,11 @@
+package inheritance1;
+
+public class hasil {
+
+    public static void main(String[] args) {
+        anak dina=new anak();
+        dina.setnama("Dinaa");
+        dina.Cetak();
+    }
+   
+}
